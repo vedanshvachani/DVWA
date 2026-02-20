e37ac4b3d37435dcc7840179de9f9c79d18553a6
vedanshvachani <vedanshvachani16@gmail.com>
2026-02-20 12:23:28 +0530

    Create admin_level.php

---
+++ admin_level.php
@@ -0,0 +1,20 @@
+<?php
+if ($_GET['is_admin'] == 1) {
+    echo "Welcome Admin!";
+}
+?>
+
+
+<?php
+$comment = $_POST['comment'];
+file_put_contents("comments.txt", $comment . PHP_EOL, FILE_APPEND);
+echo "Comment saved!";
+?>
+
+
+<?php
+move_uploaded_file(
+    $_FILES['file']['tmp_name'],
+    "uploads/" . $_FILES['file']['name']
+);
+?>
