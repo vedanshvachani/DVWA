9fb6b6220121fb8efed85ae9a02a6720c6d29970
vedanshvachani <vedanshvachani16@gmail.com>
2026-03-27 15:49:08 +0530

    Add reflected XSS example in test 27032026.php
    Added vulnerable reflected XSS example using direct echo.

--- test 27032026.php
+++ test 27032026.php
@@ -4,4 +4,11 @@ $id = $_GET['id'];
 $query = "SELECT * FROM users WHERE id = " . $id;
 $result = $conn->query($query);
 
+
+
+
+// VULNERABLE: Reflected XSS via direct echo
+$name = $_GET['name'];
+echo "<h1>Welcome, " . $name . "</h1>";
+
 ?>
