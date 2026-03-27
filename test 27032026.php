9ed1e731df3282be844a7b49e2cf20be89b15c63
vedanshvachani <vedanshvachani16@gmail.com>
2026-03-27 15:52:50 +0530

    Remove vulnerable XSS code from test 27032026.php
    Removed PHP code that was vulnerable to reflected XSS.

--- test 27032026.php
+++ test 27032026.php
@@ -1,14 +1,1 @@
-<?php
-
-$id = $_GET['id'];
-$query = "SELECT * FROM users WHERE id = " . $id;
-$result = $conn->query($query);
-
-
-
-
-// VULNERABLE: Reflected XSS via direct echo
-$name = $_GET['name'];
-echo "<h1>Welcome, " . $name . "</h1>";
-
-?>
+hello
